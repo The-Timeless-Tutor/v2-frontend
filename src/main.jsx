b51f961b49bd797230cf84592db71dd170ac2847
13x54n <13x54r@gmail.com>
2024-04-27 04:01:51 -0400

    Minor Branch Issue Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import Router from "./utils/Routers.jsx";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <>
+    <Router />
+  </>
+);
